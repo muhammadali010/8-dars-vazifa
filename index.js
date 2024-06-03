82ecea83950802d0e9596508bbf181d82f962bac
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-06-03 18:26:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,174 @@
+// 1chi masala
+// let array = [1, 2, 3, 4, 5];
+// function multiply(arg) {
+//   let sum = 0;
+//   for (let i = 0; i < arg.length; i++) {
+//     sum = sum + arg[i];
+//   }
+// return sum;
+// }
+// console.log(multiply(array));
+
+
+// 2chi masala
+// let array = [1,2, 3, 5, 7,];
+// function multiply(arg) {
+//   let sum = 0;
+//   for (let i = 0; i < arg.length; i++) {
+//     if (arr[0] > array[1] && array[0] > array[2]) {
+//       console.log(array[0]);
+//     } else if (array[1] > array[0] && array[1] > array[2]) {
+//       console.log(array[1]);
+//     } else {
+//       console.log(array[2]);
+//     }
+//   }
+//   return sum;
+// }
+// console.log(multiply(array));
+
+
+// 3chi masala
+// let array = [1, 2, 3, 4,];
+// function multiply(arg) {
+//   let sum = 0;
+//   for (let i = 0; i < arg.length; i++) {
+//     if (arr[0] < array[1] && array[0] < array[2]) {
+//       console.log(arr[0]);
+//     } else if (array[1] < array[0] && array[1] < array[2]) {
+//       console.log(array[1]);
+//     } else {
+//       console.log(array[2]);
+//     }
+//   }
+//   return sum;
+// }
+// console.log(multiply(array));
+
+
+// 5chi masala
+// let arr = [1,2,3,4,5];
+// function two(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     array[i] = array[i] * 2;
+//   }
+//   return array;
+// }
+// console.log(two(arr));
+
+
+// 6chi masala
+// let arr = [1,2,3,4,5];
+// function juftSon(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 == 0) {
+//       array[i] = null
+//     }
+//   }
+//   return array;
+// }
+// console.log(juftSon(arr));
+
+
+// 7chi masala
+// let arr = [1,2,3,4];
+// function toqSon(array) {
+//   for (let i = 0; i < array.length; i++) {
+// if (array[i] % 2 == 1) {
+//   array[i] = null
+// }
+//   }
+//   return array;
+// }
+// console.log(toqSon(arr));
+
+
+// 8chi masala
+// let arr = [1, 2, 3, 4];
+// let result= 1;
+// function mavjud(array, result) {
+//   return array.includes(result);
+// }
+// console.log(result);
+// console.log(mavjud(arr, result));
+
+
+// 9chi masala
+// let a = [1, 2, 3, 4, 5];
+// function teskarisi(array) {
+//   return array.reverse();
+// }
+// console.log(teskarisi(a));
+
+
+// 10chi masala
+// let arr = [1, 2, 3, 4, 5];
+// let qiymati = 25;
+// function kattasi(array, qiymati) {
+//   let count = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] > qiymati) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// console.log(arr);
+// console.log(kattasi(arr, qiymati));
+
+
+// 11chi masala
+// let arr = [1, 2, 3, 4, 5];
+// function Oshirish(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     array[i]++
+//   }
+//   return array;
+// }
+// console.log(Oshirish(arr));
+
+
+// 12chi masala
+// let array = [1,2,3,4,5];
+// let result = 30;
+// function index(result, array) {
+//   return array.indexOf(result);
+// }
+// console.log(array.indexOf(element));
+
+
+// 14chi masala
+// let array = [1,2,3,4,5];
+// function musbat(array) {
+//   let musbatlar = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] > 0) {
+//       musbatlar.push(array[i]);
+//     }
+//   }
+//   return musbatlar;
+// }
+// console.log(musbat(array));
+
+
+// 15chi masala
+// let array = [1, 2, 3, 4, 5, 6, 7];
+// function manfiy(array) {
+//   let manfiylar = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] < 0) {
+//       manfiylar.push(array[i]);
+//     }
+//   }
+//   return manfiylar;
+// }
+// console.log(manfiy(array));
+
+
+// 16chi masala
+// let array = [1, 2, 3, 4, 5];
+// function elements(array) {
+//   return array.join(', ');
+// }
+// console.log(elements(array));
